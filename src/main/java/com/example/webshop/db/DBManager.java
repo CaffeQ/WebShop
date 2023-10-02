515c0ce6ded06ed2d0cccb2548aa51576f49f06d
CaffeQ <102230900+CaffeQ@users.noreply.github.com>
2023-10-02 13:55:56 +0200

    attempt to connect to db

--- src/main/java/com/example/webshop/db/DBManager.java
+++ src/main/java/com/example/webshop/db/DBManager.java
@@ -1,5 +1,24 @@
 package com.example.webshop.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBManager{
+    private static Connection con;
+    public static Connection getConnection(){
+        if(con != null)
+            return con;
+        Connection newCon = null;
+        try {
+             newCon = DriverManager.getConnection("jdbc:mysql://localhost" +
+                    "user=root&password=123");
+        } catch (SQLException e){
+            System.out.println("Could not connect to SQL server");
+            e.printStackTrace();
+        }
+        con = newCon;
+        return con;
+    }
 
 }
