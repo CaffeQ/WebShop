7d0536095f37e00f09083d9f055e0c8453446023
tattack99 <62747925+tattack99@users.noreply.github.com>
2023-10-02 13:41:58 +0200

    added sqlDB

---
+++ src/main/java/com/example/webshop/db/db.sql
@@ -0,0 +1,31 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS workshop;
+
+-- Create the database
+CREATE DATABASE workshop;
+
+-- Use the database
+USE workshop;
+
+-- Create the users table
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+-- Insert sample users
+INSERT INTO users (username, password) VALUES ('alice', 'password123');
+INSERT INTO users (username, password) VALUES ('bob', 'bobsecure');
+INSERT INTO users (username, password) VALUES ('carol', 'carolpass');
+
+-- Create the 'item' table
+CREATE TABLE item (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+-- Insert sample items
+INSERT INTO item (name, description) VALUES ('Laptop', 'A portable computer.');
+INSERT INTO item (name, description) VALUES ('Smartphone', 'A mobile device with advanced features.');
+INSERT INTO item (name, description) VALUES ('Chair', 'A piece of furniture for sitting.');
+
